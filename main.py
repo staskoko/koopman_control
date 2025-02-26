bbb2e5cf1edea2d4fb55aa36c9f3e08b3385ed33
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-02-26 11:30:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,10 @@ gen_type = 0
 
 [train_tensor, test_tensor, val_tensor] = DataGenerator(x1range, x2range, numICs, tSpan, mu, lam, file_path, gen_type)
 
+print(f"Train tensor shape: {train_tensor.shape}")
+print(f"Test tensor shape: {test_tensor.shape}")
+print(f"Validation tensor shape: {val_tensor.shape}")
+
 # NN Structure
 
 Num_meas = 2
@@ -49,14 +53,14 @@ model = AUTOENCODER(Num_meas, Num_Obsv, Num_Neurons, Num_hidden_encoder, Num_hid
 
 # Training Loop
 
-eps = 1000        # Number of epochs per batch size
+eps = 2        # Number of epochs per batch size
 lr = 1e-3        # Learning rate
 batch_size = 256
 S_p = 30
 T = len(train_tensor[0, :, :])
 alpha = [0.1, 10e-7, 10e-15]
 W = 0
-M = 3 # Amount of models you want to run
+M = 1 # Amount of models you want to run
 
 [Lowest_loss, Lowest_test_loss, Best_Model] = trainingfcn(eps, lr, batch_size, S_p, T, alpha, W, Num_meas, Num_Obsv, Num_Neurons, Num_hidden_encoder, Num_hidden_decoder, train_tensor, test_tensor, M)
 
