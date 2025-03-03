d8a24119099191dfdf5619bf9486da57bb3d0618
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-03-03 09:19:46 +0100

    Update nn_structure.py

--- nn_structure.py
+++ nn_structure.py
@@ -11,23 +11,23 @@ class AUTOENCODER(nn.Module):
         self.x_Encoder_In = nn.Linear(Num_meas, Num_x_Neurons)
         self.x_encoder_hidden = nn.ModuleList([nn.Linear(Num_x_Neurons, Num_x_Neurons) for _ in range(Num_hidden_x_encoder)])
         self.x_Encoder_out = nn.Linear(Num_x_Neurons, Num_x_Obsv)
-        
+
         self.x_Koopman = nn.Linear(Num_x_Obsv, Num_x_Obsv, bias=False)
-        
+
         self.x_Decoder_In = nn.Linear(Num_x_Obsv, Num_x_Neurons)
         self.x_decoder_hidden = nn.ModuleList([nn.Linear(Num_x_Neurons, Num_x_Neurons) for _ in range(Num_hidden_x_decoder)])
         self.x_Decoder_out = nn.Linear(Num_x_Neurons, Num_meas)
-        
+
         self.u_Encoder_In = nn.Linear(Num_meas + Num_inputs, Num_u_Neurons)
         self.u_encoder_hidden = nn.ModuleList([nn.Linear(Num_u_Neurons, Num_u_Neurons) for _ in range(Num_hidden_u_encoder)])
         self.u_Encoder_out = nn.Linear(Num_u_Neurons, Num_u_Obsv)
-        
+
         self.u_Koopman = nn.Linear(Num_u_Obsv, Num_u_Obsv, bias=False)
-        
+
         self.u_Decoder_In = nn.Linear(Num_u_Obsv, Num_u_Neurons)
         self.u_decoder_hidden = nn.ModuleList([nn.Linear(Num_u_Neurons, Num_u_Neurons) for _ in range(Num_hidden_u_decoder)])
         self.u_Decoder_out = nn.Linear(Num_u_Neurons, Num_meas + Num_inputs)
-        
+
         self._init_weights()
 
     def _init_weights(self):
@@ -74,8 +74,8 @@ class AUTOENCODER(nn.Module):
 
     def forward(self, x_k):
         y_k = self.x_Encoder(x_k)
-        u_hat = self.u_Encoder(x_k)
-        y_k1 = self.x_Koopman_op(y_k) + self.u_Koopman_op(u_hat)
+        v_k = self.u_Encoder(x_k)
+        y_k1 = self.x_Koopman_op(y_k) + self.u_Koopman_op(v_k)
         x_k1 = self.x_Decoder(y_k1)
         return x_k1
 
