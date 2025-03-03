848524e0a55c45e79416a648ad5a90fb6797491e
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-03-03 09:25:50 +0100

    Update help_func.py

--- help_func.py
+++ help_func.py
@@ -1,1 +1,22 @@
+import torch
+import torch.nn.functional as F
 
+
+def self_feeding(model, xuk, Num_meas):
+    initial_input = xuk[:, 0, :]
+    num_steps = int(len(xuk[0, :, 0]))
+    inputs = xuk[:,:,Num_meas:]
+
+    predictions = []
+    predictions.append(initial_input)
+
+    for step in range(num_steps - 1):
+        x_pred = model(initial_input)
+        x_pred = torch.cat((x_pred, inputs[:, step, :]), dim=1)
+        predictions.append(x_pred.detach())
+        initial_input = x_pred
+
+    predictions = torch.stack(predictions, dim=1)
+    loss = custom_loss(predictions, xuk)
+
+    return predictions, loss
