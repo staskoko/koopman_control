5a42f3545dc56a6e6478d9861764532b3613c9b2
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-03-04 11:30:07 +0100

    Update plotting_script.py

--- plotting_script.py
+++ plotting_script.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from plotting import plot_results
 
 plot_results(Lgx_Array, Lgu_Array, L3_Array, L4_Array, L5_Array, L6_Array,
              Lowest_test_loss_index, model, val_tensor, train_tensor,
