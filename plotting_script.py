8a8c25bd16482a0577fd7aba8383b838755eef9d
Berako00 <126508321+Berako00@users.noreply.github.com>
2025-03-05 08:44:03 +0100

    Delete plotting_script.py

--- plotting_script.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import torch
-from plotting import plot_results
-
-plot_results(Lgx_Array, Lgu_Array, L3_Array, L4_Array, L5_Array, L6_Array,
-             Lowest_test_loss_index, model, val_tensor, train_tensor,
-             Num_meas, r, enc_self_feeding)
